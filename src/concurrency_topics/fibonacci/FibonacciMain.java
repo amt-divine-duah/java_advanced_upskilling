b6c60dd3e35475abe834a80a616091812284eed1
Divine Duah <divine.duah@amalitech.com>
2024-03-14 14:51:37 +0000

    feat: implement fibonacci sequence

---
+++ src/concurrency_topics/fibonacci/FibonacciMain.java
@@ -0,0 +1,13 @@
+package concurrency_topics.fibonacci;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class FibonacciMain {
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+        int number = 40;
+        FibonacciSequence task = new FibonacciSequence(number);
+        int result = pool.invoke(task);
+        System.out.println("Fibonacci of " + number + " is " + result);
+    }
+}
---
+++ src/concurrency_topics/fibonacci/FibonacciSequence.java
@@ -0,0 +1,35 @@
+/**
+ * Consider a task to calculate the Fibonacci sequence of a large number.
+ * This can be implemented as a ForkJoinTask where the task recursively
+ * splits the calculation into smaller subproblems (calculating Fibonacci of smaller numbers)
+ * until the base cases (numbers 1 and 0) are reached. The final results from these
+ * subtasks are then combined to get the final Fibonacci number.
+ *
+ * The Fibonacci sequence is defined as follows:
+ * F(number) = F(number-1) + F(number-2), where F(0) = 0 and F(1) = 1
+ */
+package concurrency_topics.fibonacci;
+
+import java.util.concurrent.RecursiveTask;
+
+class FibonacciSequence extends RecursiveTask<Integer> {
+
+    private final int number;
+
+    public FibonacciSequence(int number) {
+        this.number = number;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (number <= 1) {
+            return number;
+        }
+        FibonacciSequence task1 = new FibonacciSequence(number - 1);
+        task1.fork();
+        FibonacciSequence task2 = new FibonacciSequence(number - 2);
+        task2.fork();
+        return task1.join() + task2.join();
+    }
+}
+
