b895f8166e2c49c1bcb068ba4860ac7e2bb6fde0
Divine Duah <divine.duah@amalitech.com>
2024-03-18 06:40:51 +0000

    feat: implement array summation using forkjoinpool

---
+++ src/concurrency_topics/array_summation/ArraySummation.java
@@ -0,0 +1,33 @@
+/**
+ * Parallel Summation of an Array (Increased Complexity)
+ */
+package concurrency_topics.array_summation;
+
+import java.util.Arrays;
+import java.util.concurrent.RecursiveTask;
+
+import static java.util.Arrays.copyOfRange;
+
+public class ArraySummation extends RecursiveTask<Long> {
+    private long[] array;
+
+    public ArraySummation(long[] array) {
+        this.array = array;
+    }
+
+    @Override
+    protected Long compute() {
+        // base case
+        if (array.length <= 1000) {
+            // sum the array using stream
+            return Arrays.stream(array).sum();
+        }
+        // recursive case
+        int mid = array.length / 2;
+        ArraySummation task1 = new ArraySummation(copyOfRange(array, 0, mid));
+        task1.fork();
+        ArraySummation task2 = new ArraySummation(copyOfRange(array, mid, array.length));
+        task2.fork();
+        return task1.join() + task2.join();
+    }
+}
---
+++ src/concurrency_topics/array_summation/ArraySummationMain.java
@@ -0,0 +1,25 @@
+package concurrency_topics.array_summation;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+
+public class ArraySummationMain {
+    public static void main(String[] args) {
+
+        ForkJoinPool pool = new ForkJoinPool();
+
+        Random random = new Random();
+        int size = 10000;
+        long[] array = new long[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextLong(0,20);
+        }
+
+        ArraySummation task = new ArraySummation(array);
+        Long result = pool.invoke(task);
+
+        System.out.println("This is the result " + result);
+    }
+}
