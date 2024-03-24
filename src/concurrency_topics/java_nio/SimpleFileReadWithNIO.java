6f2ebd9150091d604a87cb364dfaca44719537cc
Divine Duah <divine.duah@amalitech.com>
2024-03-24 14:58:10 +0000

    feat: simple read file using NIO

---
+++ src/concurrency_topics/java_nio/SimpleFileReadWithNIO.java
@@ -0,0 +1,37 @@
+package concurrency_topics.java_nio;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
+
+public class SimpleFileReadWithNIO {
+
+    public static void main(String[] args) {
+        // File to read from
+        File file = new File("src/concurrency_topics/java_nio/data.txt");
+
+        // open file channel in read-only mode
+        try (FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+            // Allocate a byte buffer
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            // allocate a byte buffer
+            int bytesRead;
+            do {
+                // Attempt to read data into the buffer (might not read all requested bytes)
+                bytesRead = channel.read(buffer);
+
+                // Process the read data (e.g., print or write to another file)
+                if (bytesRead > 0) {
+                    // Handle data processing here
+                    System.out.println(new String(buffer.array(), 0, bytesRead));
+                    buffer.clear(); // Prepare the buffer for the next read operation
+                }
+            } while (bytesRead > 0); // Continue reading until EOF
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
