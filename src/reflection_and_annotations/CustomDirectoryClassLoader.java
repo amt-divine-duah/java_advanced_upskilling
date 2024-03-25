78397f84a545753ce63c136e372ca7423c31e313
Divine Duah <divine.duah@amalitech.com>
2024-03-25 08:43:05 +0000

    feat: implement class loader using reflection

---
+++ src/reflection_and_annotations/CustomDirectoryClassLoader.java
@@ -0,0 +1,50 @@
+package reflection_and_annotations;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class CustomDirectoryClassLoader extends ClassLoader {
+
+    private final String directoryPath;
+
+    public CustomDirectoryClassLoader(String directoryPath) {
+        this.directoryPath = directoryPath;
+    }
+
+    @Override
+    protected Class<?> findClass(String className) throws ClassNotFoundException {
+        try {
+            byte[] classBytes = loadClassBytes(className);
+            return defineClass(className, classBytes, 0, classBytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException("Failed to load class " + className, e);
+        }
+    }
+
+    private byte[] loadClassBytes(String className) throws IOException {
+        String classFilePath = directoryPath + File.separator + className.replace('.', File.separatorChar) + ".class";
+        try (InputStream inputStream = new FileInputStream(classFilePath)) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            int bytesRead;
+            byte[] buffer = new byte[4096];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            return outputStream.toByteArray();
+        }
+    }
+
+    public static void main(String[] args) {
+        String directoryPath = "C:\\Santana\\Java Tutorials\\Amalitech Upskilling\\java_advanced_upskilling\\src\\concurrency_topics\\array_summation";
+        CustomDirectoryClassLoader classLoader = new CustomDirectoryClassLoader(directoryPath);
+
+        try {
+            Class<?> loadedClass = classLoader.loadClass("concurrency_topics.array_summation.ArraySummation");
+            String simpleName = loadedClass.getSimpleName();
+            System.out.println("I have loaded " + simpleName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
